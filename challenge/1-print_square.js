7b49defaef417fe282c39150abe553f1c2bde9b4
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-09-28 21:27:14 +0200

    Task correction and improvement

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,17 +1,12 @@
-#!/usr/bin/node
-
-if (process.argv.length !== 3) {
-  console.log('Usage: ./1-print_square.js <size>');
-  process.exit(1);
-}
+#!/usr/bin/env node
 
 const size = parseInt(process.argv[2]);
 
 if (isNaN(size)) {
-  console.log('Size must be a number');
+  console.log("Missing size");
   process.exit(1);
 }
 
 for (let i = 0; i < size; i++) {
-  console.log('#'.repeat(size));
+  console.log("#".repeat(size));
 }
