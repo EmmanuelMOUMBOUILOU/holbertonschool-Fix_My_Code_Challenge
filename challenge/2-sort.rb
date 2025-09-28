add95840d8d361c40505c9a09d9664d424d47f7c
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-09-28 22:13:26 +0200

    Task correction and improvement

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,9 +1,3 @@
 #!/usr/bin/env ruby
 
-# Get all command-line arguments as strings
-args = ARGV
-
-# Sort arguments lexicographically (ASCII order)
-args.sort.each do |arg|
-  puts arg
-end
+ARGV.sort.each { |arg| puts arg }
