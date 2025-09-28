96b5f478d4764a671b2a2c7ff42a044a768bbe5f
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-09-28 21:11:49 +0200

    Task correction and improvement

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,25 +1,2 @@
 #!/usr/bin/env python3
-import sys
-
-def fizzbuzz(n):
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(i, end=" ")
-    print()  # Pour finir par un saut de ligne
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <number>")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("Please provide an integer")
-        sys.exit(1)
-    fizzbuzz(n)
+import sys;[print("FizzBuzz" if i%15==0 else "Fizz" if i%3==0 else "Buzz" if i%5==0 else i, end=" ") for i in range(1, int(sys.argv[1])+1)];print()
