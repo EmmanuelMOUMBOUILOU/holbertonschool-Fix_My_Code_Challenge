6d5a4c41c4eefaecf10bc5c460a5adb63e2dbedf
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-09-28 22:38:01 +0200

    Task correction and improvement

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,24 +1,21 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes node at given index in a doubly linked list
- * @head: pointer to pointer to head of list
- * @index: index of node to delete
+ * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t list
+ * @head: pointer to pointer to the head of the list
+ * @index: index of the node to delete
  *
  * Return: 1 if success, -1 if fail
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *tmp;
+    dlistint_t *tmp = *head;
     unsigned int i;
 
     if (head == NULL || *head == NULL)
         return (-1);
 
-    tmp = *head;
-
-    /* If deleting the head node */
     if (index == 0)
     {
         *head = tmp->next;
@@ -28,19 +25,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
         return (1);
     }
 
-    /* Traverse to the node at position index */
     for (i = 0; tmp != NULL && i < index; i++)
         tmp = tmp->next;
 
-    /* If index is out of range */
     if (tmp == NULL)
         return (-1);
 
-    /* Reconnect the previous node, if any */
     if (tmp->prev != NULL)
         tmp->prev->next = tmp->next;
 
-    /* Reconnect the next node, if any */
     if (tmp->next != NULL)
         tmp->next->prev = tmp->prev;
 
