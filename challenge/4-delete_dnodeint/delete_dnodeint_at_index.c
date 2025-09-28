950a539eec7404472c4fb8404666ab2d853c16da
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-09-28 23:58:20 +0200

    Task correction and improvement

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,37 +1,48 @@
 #include "lists.h"
 #include <stdlib.h>
-
 /**
- * delete_dnodeint_at_index - Deletes the node at index `index` of a doubly linked list.
- * @head: Pointer to the pointer to the head of the list.
- * @index: Index of the node to delete (starting at 0).
- * Return: 1 if it succeeded, -1 if it failed.
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *tmp;
-    unsigned int i;
-
-    if (!head || !*head)
-        return (-1);
-
-    tmp = *head;
-
-    for (i = 0; tmp && i < index; i++)
-        tmp = tmp->next;
-
-    if (!tmp)
-        return (-1);
-
-    if (tmp->prev)
-        tmp->prev->next = tmp->next;
-    else
-        *head = tmp->next;
-
-    if (tmp->next)
-        tmp->next->prev = tmp->prev;
-
-    free(tmp);
-
-    return (1);
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p = 0;
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && saved_head != NULL)
+	{
+		saved_head = saved_head->next;
+		p++;
+	}
+	if (saved_head == NULL)
+		return (-1);
+	/* If it's the first node */
+	if (index == 0)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+			tmp->prev = NULL;
+	}
+	else
+	{
+		/* Link previous node to next */
+		if (saved_head->prev)
+			saved_head->prev->next = saved_head->next;
+		if (saved_head->next)
+			saved_head->next->prev = saved_head->prev;
+		free(saved_head);
+	}
+	return (1);
 }
