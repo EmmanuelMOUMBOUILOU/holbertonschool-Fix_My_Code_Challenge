dbf37b5eef73dceddf80f78250cd7903e526377f
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-09-28 21:13:13 +0200

    Task correction and improvement

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
-import sys;[print("FizzBuzz" if i%15==0 else "Fizz" if i%3==0 else "Buzz" if i%5==0 else i, end=" ") for i in range(1, int(sys.argv[1])+1)];print()
+import sys
+
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
+    print()
